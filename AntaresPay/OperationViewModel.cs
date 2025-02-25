2aa5c4c68e924419696789dec140399e9868df6f
lucksma96 <lucksma96@users.noreply.github.com>
2025-02-25 17:48:57 -0300

    chore: created operation viewmodel

---
+++ AntaresPay/OperationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AntaresPay;
+
+[QueryProperty("Operation", "Operation")]
+public partial class OperationViewModel : ObservableObject
+{
+    [ObservableProperty]
+    public partial string? Operation { get; set; }
+
+    [RelayCommand]
+    private static async Task Return() => await Shell.Current.GoToAsync("..");
+}
