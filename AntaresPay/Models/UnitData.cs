b5e7a9569f72e5b7e1ba48d256c4835336febe0b
lucksma96 <lucksma96@users.noreply.github.com>
2025-02-26 23:37:10 -0300

    chore: create unit model

---
+++ AntaresPay/Models/UnitData.cs
@@ -0,0 +1,8 @@
+﻿namespace AntaresPay.Models;
+
+public class UnitData
+{
+    public required string Name { get; set; }
+    public required string Id { get; set; }
+    public int Balance { get; set; }
+}
