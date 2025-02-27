2a44c74257a1055ef0d755f7c71d6818c5e6edd4
lucksma96 <lucksma96@users.noreply.github.com>
2025-02-26 23:37:49 -0300

    chore: navigation from operation to transaction

--- AntaresPay/OperationViewModel.cs
+++ AntaresPay/OperationViewModel.cs
@@ -7,8 +7,14 @@ namespace AntaresPay;
 public partial class OperationViewModel : ObservableObject
 {
     [ObservableProperty]
-    public partial string? Operation { get; set; }
+    public partial string Operation { get; set; }
+
+    [ObservableProperty]
+    public partial int Value { get; set; }
 
     [RelayCommand]
     private static async Task Return() => await Shell.Current.GoToAsync("..");
+
+    [RelayCommand]
+    private async Task GoToTransaction() => await Shell.Current.GoToAsync($"{nameof(TransactionPage)}?{nameof(Operation)}={Operation}");
 }
