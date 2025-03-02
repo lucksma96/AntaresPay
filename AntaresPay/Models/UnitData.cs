53ee4f5145a5cfe0867dfeed6660c38bbc6ddf2b
lucksma96 <lucksma96@users.noreply.github.com>
2025-03-01 22:39:24 -0300

    chore: unitdata validation

--- AntaresPay/Models/UnitData.cs
+++ AntaresPay/Models/UnitData.cs
@@ -2,7 +2,26 @@
 
 public class UnitData
 {
+    private List<string> units =
+    [
+        "phoenix",
+        "scorpion",
+        "andromeda",
+        "europa",
+        "hercules",
+        "alpha",
+        "lyra",
+        "shaula",
+        "bellatrix",
+        "pegasus"
+    ];
+
     public required string Name { get; set; }
     public required string Id { get; set; }
     public int Balance { get; set; }
+
+    public bool IsValidUnit()
+    {
+        return units.Contains(Id);
+    }
 }
