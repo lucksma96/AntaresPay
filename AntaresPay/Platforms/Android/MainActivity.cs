79f54326c3b529e9202216eadb256cdc78a60685
lucksma96 <lucksma96@users.noreply.github.com>
2025-03-02 14:08:04 -0300

    i guess this is required for nfc to work

--- AntaresPay/Platforms/Android/MainActivity.cs
+++ AntaresPay/Platforms/Android/MainActivity.cs
@@ -1,11 +1,30 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.NFC;
 
 namespace AntaresPay
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            CrossNFC.Init(this);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            CrossNFC.OnResume();
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            CrossNFC.OnNewIntent(intent);
+        }
     }
 }
