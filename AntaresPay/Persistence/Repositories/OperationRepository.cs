75601928efa2de9d1a3789745fe68904317bfe7a
lucksma96 <lucksma96@users.noreply.github.com>
2025-04-06 00:51:05 -0300

    feat: created operation repository

---
+++ AntaresPay/Persistence/Repositories/OperationRepository.cs
@@ -0,0 +1,7 @@
+﻿using AntaresPay.Persistence.Entities;
+
+namespace AntaresPay.Persistence.Repositories;
+
+public class OperationRepository(SQLiteDatabase database) : BaseRepository<OperationEntity>(database)
+{
+}
