7c9c1e680c66b00546d15ffd1bc8611436dc99dd
lucksma96 <lucksma96@users.noreply.github.com>
2025-04-06 00:50:44 -0300

    fix: repository interface return types

--- AntaresPay/Persistence/Repositories/IRepository.cs
+++ AntaresPay/Persistence/Repositories/IRepository.cs
@@ -4,7 +4,7 @@ public interface IRepository<Entity>
 {
     Task<List<Entity>> GetAllAsync();
     Task<Entity> GetByIdAsync(int id);
-    Task AddAsync(Entity entity);
-    Task UpdateAsync(Entity entity);
-    Task DeleteAsync(int id);
+    Task<int> AddAsync(Entity entity);
+    Task<int> UpdateAsync(Entity entity);
+    Task<int> DeleteAsync(int id);
 }
