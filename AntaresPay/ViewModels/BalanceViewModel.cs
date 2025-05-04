997ba91fbc90529edd17609cf0ec81978c55d76b
lucksma96 <lucksma96@users.noreply.github.com>
2025-05-03 23:43:49 -0300

    chore: load operations in balance viewmodel

--- AntaresPay/ViewModels/BalanceViewModel.cs
+++ AntaresPay/ViewModels/BalanceViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using AntaresPay.Models;
+using AntaresPay.Persistence.Entities;
+using AntaresPay.Persistence.Repositories;
+using CommunityToolkit.Maui.Core.Extensions;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace AntaresPay.ViewModels;
 
-public partial class BalanceViewModel : ObservableObject
+public partial class BalanceViewModel(OperationRepository operationRepository) : ObservableObject
 {
+    private readonly OperationRepository _operationRepository = operationRepository;
+
     [ObservableProperty]
-    public partial UnitData UnitData { get; set; }
+    public partial UnitData? UnitData { get; set; }
 
     [ObservableProperty]
     public partial bool IsDeviceListening { get; set; }
@@ -17,4 +23,10 @@ public partial class BalanceViewModel : ObservableObject
 
     [RelayCommand]
     private static async Task Return() => await Shell.Current.GoToAsync("..");
+
+    [RelayCommand]
+    private async Task LoadOperations(string unitName) => Operations = (await _operationRepository.GetByUnitNameAsync(unitName)).ToObservableCollection();
+
+    [ObservableProperty]
+    public partial ObservableCollection<OperationEntity> Operations { get; set; } = [];
 }
