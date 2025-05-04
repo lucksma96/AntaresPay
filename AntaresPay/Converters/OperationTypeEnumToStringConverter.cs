c456ad032f0df37b1574262a1c73d2df54727f78
lucksma96 <lucksma96@users.noreply.github.com>
2025-05-04 00:05:34 -0300

    chore: operationtype enum converter

---
+++ AntaresPay/Converters/OperationTypeEnumToStringConverter.cs
@@ -0,0 +1,31 @@
+﻿using AntaresPay.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntaresPay.Converters
+{
+    public class OperationTypeEnumToStringConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is OperationTypeEnum operationType)
+            {
+                return operationType switch
+                {
+                    OperationTypeEnum.Payment => "Pagamento",
+                    OperationTypeEnum.Charge => "Cobrança",
+                    _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+                };
+            }
+            return string.Empty;
+        }
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
